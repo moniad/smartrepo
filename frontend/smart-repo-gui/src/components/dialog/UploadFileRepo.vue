37ba4d981e8b918cba482563db132f06b8f3f33e
skrzek <skrzek@student.agh.edu.pl>
2021-03-29 19:29:13 +0200

    mockup checkpoint

---
+++ frontend/smart-repo-gui/src/components/dialog/UploadFileRepo.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <dialog
+        v-if="active"
+        :outlined="true"
+        title="Upload Service Data"
+    >
+      <template slot="content">
+        <file-uploader
+            @file-upload="(file) => loadFile(file)"
+            file-types=".xlsx,.csv"
+        />
+      </template>
+      <template slot="actions">
+        <v-spacer />
+        <v-btn plain label="Cancel" @button-pressed="cancel" />
+        <v-btn
+            @button-pressed="
+            onUpload();
+            active = false;
+          "
+            label="Confirm"
+        />
+      </template>
+    </dialog>
+  </div>
+</template>
+
+<script>
+import FileUploader from "@/components/utils/FileUploader";
+// import Dialog from "@/components/utils/Dialog";
+
+export default {
+  name: "UploadFileRepo",
+  data() {
+    return { file: null, active: true };
+  },
+  components: {
+     FileUploader
+  },
+  methods: {
+    loadFile(file) {
+      console.log(file)
+    },
+    onUpload() {
+    },
+    cancel() {
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
--- frontend/smart-repo-gui/src/plugins/vuetify.js
+++ frontend/smart-repo-gui/src/plugins/vuetify.js
@@ -1,7 +1,23 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
+import Vue from "vue";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+import "@mdi/font/css/materialdesignicons.css";
 
 Vue.use(Vuetify);
-
 export default new Vuetify({
+    icons: {
+        iconfont: "mdi", // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4' || 'faSvg'
+    },
+    theme: {
+        themes: {
+            light: {
+                primary: '#000000',
+                secondary: '#ffffff',
+                background: '#f9f9f9',
+                dark : '#000000',
+                light: '#ffffff',
+                progress: '#000000',
+            },
+        },
+    },
 });
