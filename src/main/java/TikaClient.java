71f456955d1d83d7a2f7d5f350b0b50cebc6ef31
Jakub Caputa <jjakubcaputa@gmail.com>
2021-04-12 21:50:07 +0200

    Added longer performance tests

--- src/main/java/TikaClient.java
+++ src/main/java/TikaClient.java
@@ -4,31 +4,38 @@ import org.apache.tika.langdetect.optimaize.OptimaizeLangDetector;
 import org.apache.tika.language.detect.LanguageDetector;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.parser.pdf.PDFParserConfig;
 import org.apache.tika.sax.BodyContentHandler;
+import org.apache.tika.sax.LinkContentHandler;
+import org.xml.sax.ContentHandler;
 import org.xml.sax.SAXException;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 
+
 public class TikaClient {
-    private static final String recipiesFilename = "przepisyChodakowska.pdf";
-    private static final String cppFilename = "cpp17.pdf";
+    private static final String recipiesFilename = "pdf_files/przepisyChodakowska91.pdf";
 
     public static void main(String[] args) throws IOException, TikaException, SAXException {
-        long startTime = System.nanoTime();
 
-        String parsedText = parseToPlainText(cppFilename);
+        File dir = new File("pdf_files/");
+        File[] directoryListing = dir.listFiles();
+        if (directoryListing != null) {
+            for (File child : directoryListing) {
 
-        long endTime = System.nanoTime();
-        long timeElapsed = endTime - startTime;
+                long startTime = System.nanoTime();
 
-        System.out.println(parsedText);
-        System.out.println("LANGUAGE: " + identifyLanguage(parsedText));
+                String parsedText = parseToPlainText(child.toString());
 
-        System.out.println("Execution time in nanoseconds  : " + timeElapsed);
-        System.out.println("Execution time in milliseconds : " +
-                timeElapsed / 1000000);
+                long endTime = System.nanoTime();
+                long timeElapsed = endTime - startTime;
+
+                System.out.println("Execution time in seconds : " +
+                        timeElapsed / 1000000000);
+            }
+        }
     }
 
     private static String identifyLanguage(String text) throws IOException {
@@ -40,8 +47,8 @@ public class TikaClient {
 
     private static String parseToPlainText(String filename) throws IOException, SAXException, TikaException {
         // writeLimit set to -1 disables the write limit. It can be set to 12345657 as well.
-        BodyContentHandler handler = new BodyContentHandler(-1);
-
+        //BodyContentHandler handler = new BodyContentHandler(-1);
+        ContentHandler handler = new LinkContentHandler();
         AutoDetectParser parser = new AutoDetectParser();
         Metadata metadata = new Metadata();
 
