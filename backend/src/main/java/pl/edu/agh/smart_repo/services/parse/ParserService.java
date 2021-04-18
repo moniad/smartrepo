b1a30bba1a8beb463449d3a084a26d8e0df79d61
tomsia1 <kuba.tomsia@wp.pl>
2021-04-18 21:47:31 +0200

    Split functionality of saving file and communication with parsers into separate services

---
+++ backend/src/main/java/pl/edu/agh/smart_repo/services/parse/ParserService.java
@@ -0,0 +1,87 @@
+package pl.edu.agh.smart_repo.services.parse;
+
+import com.rabbitmq.client.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.edu.agh.smart_repo.common.file.AcceptableFileExtensions;
+import pl.edu.agh.smart_repo.configuration.ConfigurationFactory;
+import pl.edu.agh.smart_repo.services.file.FileService;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@Slf4j
+@Service
+public class ParserService {
+    Channel channel;
+
+    @Autowired
+    FileService fileService;
+
+    public ParserService(ConfigurationFactory configurationFactory) throws Exception
+    {
+        //RabbitMQ settings
+        int port = 5672;
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(configurationFactory.getRabbitHost());
+        factory.setPort(port);
+        Connection connection = retryConnection(factory);
+        channel = connection.createChannel();
+
+        for (AcceptableFileExtensions extension: AcceptableFileExtensions.values())
+            channel.queueDeclare(extension.toString(), false, false, false, null);
+        //END: RabbitMQ setting
+    }
+
+    public String parse(File file, String path_relative_to_storage)
+    {
+        try {
+            String reply_to = channel.queueDeclare().getQueue();
+
+            AMQP.BasicProperties props = new AMQP.BasicProperties
+                    .Builder()
+                    .replyTo(reply_to)
+                    .build();
+
+            String extension = fileService.getExtension(file);
+            if (extension == null)
+            {
+                log.error("error while checking file extension");
+                return null;
+            }
+
+            channel.basicPublish("", extension, props, path_relative_to_storage.getBytes(StandardCharsets.UTF_8));
+
+            GetResponse response = null;
+            while (response == null)
+                response= channel.basicGet(reply_to, true);
+
+            channel.queueDelete(reply_to);
+
+            return new String(response.getBody(), StandardCharsets.UTF_8);
+
+        } catch (IOException e) {
+            log.error("error while sending file to parse queues");
+            return null;
+        }
+    }
+
+    private static Connection retryConnection(ConnectionFactory factory) throws Exception
+    {
+        int connectionTrials = 5;
+        int waitTime = 5000;
+        for (int retry = 0; retry <= connectionTrials; ++retry) {
+            try {
+                Thread.sleep(retry * waitTime);
+                return factory.newConnection();
+            } catch (IOException e) {
+                log.info("Unable to establish connection to RabbitMQ. Trial: " + retry);
+            }
+        }
+        log.info("Unable to establish connection");
+        throw new IllegalStateException("No RabbitMQConnection provided");
+    }
+
+}
--- backend/src/main/java/pl/edu/agh/smart_repo/services/upload/FileUploadService.java
+++ backend/src/main/java/pl/edu/agh/smart_repo/services/upload/FileUploadService.java
@@ -1,13 +1,10 @@
 package pl.edu.agh.smart_repo.services.upload;
 
-import com.rabbitmq.client.AMQP.BasicProperties;
-import com.rabbitmq.client.GetResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import pl.edu.agh.smart_repo.configuration.ConfigurationFactory;
-import pl.edu.agh.smart_repo.common.file.AcceptableFileExtensions;
 import pl.edu.agh.smart_repo.common.results.Result;
 import pl.edu.agh.smart_repo.common.results.ResultType;
 
@@ -15,71 +12,22 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
-import pl.edu.agh.smart_repo.services.file.FileService;
+import pl.edu.agh.smart_repo.services.parse.ParserService;
 
 @Slf4j
 @Service
 public class FileUploadService {
     private final Path storagePath;
-    Channel channel;
 
     @Autowired
-    FileService fileService;
+    ParserService parserService;
 
-    public FileUploadService(ConfigurationFactory configurationFactory) throws Exception {
-        this.storagePath = configurationFactory.getStoragePath();
-
-        //RabbitMQ settings
-        int port = 5672;
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost(configurationFactory.getRabbitHost());
-        factory.setPort(port);
-        Connection connection = retryConnection(factory);
-        channel = connection.createChannel();
-
-        for (AcceptableFileExtensions extension: AcceptableFileExtensions.values())
-            channel.queueDeclare(extension.toString(), false, false, false, null);
-        //END: RabbitMQ setting
-    }
-
-    private String parseWithParsers(File file, String path_relative_to_storage)
+    public FileUploadService(ConfigurationFactory configurationFactory)
     {
-        try {
-            String reply_to = channel.queueDeclare().getQueue();
-
-            BasicProperties props = new BasicProperties
-                    .Builder()
-                    .replyTo(reply_to)
-                    .build();
-
-            String extension = fileService.getExtension(file);
-            if (extension == null)
-            {
-                log.error("error while checking file extension");
-                return null;
-            }
-
-            channel.basicPublish("", extension, props, path_relative_to_storage.getBytes(StandardCharsets.UTF_8));
-
-            GetResponse response = null;
-            while (response == null)
-                response= channel.basicGet(reply_to, true);
-
-            channel.queueDelete(reply_to);
-
-            return new String(response.getBody(), StandardCharsets.UTF_8);
-
-        } catch (IOException e) {
-            log.error("error while sending file to parse queues");
-            return null;
-        }
+        storagePath = configurationFactory.getStoragePath();
     }
 
     public Result processFile(MultipartFile file) {
@@ -103,7 +51,7 @@ public class FileUploadService {
             return new Result(ResultType.FAILURE, e);
         }
 
-        String parsed = parseWithParsers(new_file, path_relative_to_storage);
+        String parsed = parserService.parse(new_file, path_relative_to_storage);
         if (parsed == null)
         {
             return new Result(ResultType.FAILURE, "failed to parse file");
@@ -113,20 +61,5 @@ public class FileUploadService {
 
         //TODO send to indexing service there...
         return new Result(ResultType.SUCCESS);
-    }
-
-    private static Connection retryConnection(ConnectionFactory factory) throws Exception {
-        int connectionTrials = 5;
-        int waitTime = 5000;
-        for (int retry = 0; retry <= connectionTrials; ++retry) {
-            try {
-                Thread.sleep(retry * waitTime);
-                return factory.newConnection();
-            } catch (IOException e) {
-                log.info("Unable to establish connection to RabbitMQ. Trial: " + retry);
-            }
-        }
-        log.info("Unable to establish connection");
-        throw new IllegalStateException("No RabbitMQConnection provided");
     }
 }
