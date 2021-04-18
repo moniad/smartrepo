eecc3d63c5b5b29a557f5f321d2787c076155bb4
Tomasz Skrzek <tomasz.skrzek.98@gmail.com>
2021-04-19 00:02:29 +0200

    GET files with directories non recursively

--- backend/src/main/java/pl/edu/agh/smart_repo/service/FileTreeFetcherService.java
+++ backend/src/main/java/pl/edu/agh/smart_repo/service/FileTreeFetcherService.java
@@ -1,13 +1,17 @@
 package pl.edu.agh.smart_repo.service;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.FileFilterUtils;
 import org.springframework.stereotype.Service;
 import pl.edu.agh.smart_repo.ConfigurationFactory;
 
 import java.io.File;
+import java.io.FileFilter;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class FileTreeFetcherService {
@@ -20,8 +24,10 @@ public class FileTreeFetcherService {
 
     public List<File> fetchFileTree(String directoryPath, boolean recursive, String[] extensions){
         String resultPath = Paths.get(userFilesDirectoryPath.toString(), directoryPath).toString();
-
-        List<File> files = (List<File>) FileUtils.listFiles(new File(resultPath), extensions, recursive);
+        File currentDirectory = new File(resultPath);
+        List<File> files = (List<File>) FileUtils.listFiles(currentDirectory, extensions, recursive);
+        File[] directories = Optional.ofNullable(currentDirectory.listFiles((FileFilter) FileFilterUtils.directoryFileFilter())).orElse(new File[0]);
+        files.addAll(Arrays.asList(directories));
         return files;
     }
 }
