1e7bc0c13a541b06443a03da3cb22a219652ff3b
moniad <modziedzic@student.agh.edu.pl>
2021-05-18 00:38:02 +0200

    Added space after column name.

--- parsers/xls_xlsx/src/main/kotlin/XlsXlsxParser.kt
+++ parsers/xls_xlsx/src/main/kotlin/XlsXlsxParser.kt
@@ -11,137 +11,137 @@ class XlsXlsxParser {
 
     companion object {
         @JvmStatic
-     fun main(args: Array<String>) {
+        fun main(args: Array<String>) {
 
-        val host = if (args.isNotEmpty()) args[0] else "localhost"
-        val port = 5672
+            val host = if (args.isNotEmpty()) args[0] else "localhost"
+            val port = 5672
 
-        val factory = ConnectionFactory()
-        factory.host = host
-        factory.port = port
-        val connection = retryConnection(factory)
-        val channel = connection!!.createChannel()
+            val factory = ConnectionFactory()
+            factory.host = host
+            factory.port = port
+            val connection = retryConnection(factory)
+            val channel = connection!!.createChannel()
 
-        val queueNameXls = "xls"
-        channel.queueDeclare(queueNameXls, false, false, false, null)
-        println("Waiting for messages - xls.")
+            val queueNameXls = "xls"
+            channel.queueDeclare(queueNameXls, false, false, false, null)
+            println("Waiting for messages - xls.")
 
-        val queueNameXlsx = "xlsx"
-        channel.queueDeclare(queueNameXlsx, false, false, false, null)
-        println("Waiting for messages - xlsx.")
+            val queueNameXlsx = "xlsx"
+            channel.queueDeclare(queueNameXlsx, false, false, false, null)
+            println("Waiting for messages - xlsx.")
 
-        val deliverCallbackXlsx = DeliverCallback { _, delivery ->
-            var path = String(delivery.body, Charsets.UTF_8)
-            path = "/storage/$path"
+            val deliverCallbackXlsx = DeliverCallback { _, delivery ->
+                var path = String(delivery.body, Charsets.UTF_8)
+                path = "/storage/$path"
 
-            val replyTo = delivery.properties.replyTo
-            println("Parsing path: '$path'")
+                val replyTo = delivery.properties.replyTo
+                println("Parsing path: '$path'")
 
-            var result = ""
+                var result = ""
 
-            try {
-                result = parseXlsx(path)
-            } catch (e: IOException) {
-                e.printStackTrace()
+                try {
+                    result = parseXlsx(path)
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+
+                println("\nParsed xlsx succesfully")
+                channel.basicPublish("", replyTo, null, result.toByteArray(Charsets.UTF_8))
             }
 
-            println("\nParsed xlsx succesfull")
-            channel.basicPublish("", replyTo, null, result.toByteArray(Charsets.UTF_8))
-        }
+            val deliverCallbackXls = DeliverCallback { _, delivery ->
+                var path = String(delivery.body, Charsets.UTF_8)
+                path = "/storage/$path"
 
-        val deliverCallbackXls = DeliverCallback { _, delivery ->
-            var path = String(delivery.body, Charsets.UTF_8)
-            path = "/storage/$path"
+                val replyTo = delivery.properties.replyTo
+                println("Parsing path: '$path'")
 
-            val replyTo = delivery.properties.replyTo
-            println("Parsing path: '$path'")
+                var result = ""
 
-            var result = ""
+                try {
+                    result = parseXls(path)
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
 
-            try {
-                result = parseXls(path)
-            } catch (e: IOException) {
-                e.printStackTrace()
+                println("\nParsed xls succesfully")
+                channel.basicPublish("", replyTo, null, result.toByteArray(Charsets.UTF_8))
             }
 
-            println("\nParsed xls succesfull")
-            channel.basicPublish("", replyTo, null, result.toByteArray(Charsets.UTF_8))
+            channel.basicConsume(queueNameXlsx, true, deliverCallbackXlsx) { _: String? -> }
+            channel.basicConsume(queueNameXls, true, deliverCallbackXls) { _: String? -> }
         }
 
-        channel.basicConsume(queueNameXlsx, true, deliverCallbackXlsx) { _: String? -> }
-        channel.basicConsume(queueNameXls, true, deliverCallbackXls) { _: String? -> }
-    }
+        fun parseXlsx(path: String): String {
+            val file = FileInputStream(File(path))
+            val workbook = XSSFWorkbook(file)
+            val dataFormatter = DataFormatter()
 
-    fun parseXlsx(path: String): String {
-        val file = FileInputStream(File(path))
-        val workbook = XSSFWorkbook(file)
-        val dataFormatter = DataFormatter()
+            var xlsxText = ""
 
-        var xlsxText = ""
+            for (sheet in workbook) {
 
-        for(sheet in workbook){
+                val sheetName = sheet.sheetName
+                val sheet = workbook.getSheet(sheetName)
 
-            val sheetName = sheet.sheetName
-            val sheet = workbook.getSheet(sheetName)
-
-            val row = sheet.iterator()
-            while (row.hasNext()) {
-                val currentRow = row.next()
-                val cellsInRow = currentRow.iterator()
-                while (cellsInRow.hasNext()) {
-                    val currentCell = cellsInRow.next()
-                    val cellValue = dataFormatter.formatCellValue(currentCell)
-                    xlsxText += cellValue
+                val row = sheet.iterator()
+                while (row.hasNext()) {
+                    val currentRow = row.next()
+                    val cellsInRow = currentRow.iterator()
+                    while (cellsInRow.hasNext()) {
+                        val currentCell = cellsInRow.next()
+                        val cellValue = dataFormatter.formatCellValue(currentCell)
+                        xlsxText += cellValue + " "
+                    }
                 }
             }
+            workbook.close()
+            file.close()
+            return xlsxText
         }
-        workbook.close()
-        file.close()
-        return xlsxText
-    }
 
-    fun parseXls(path: String): String{
-        val file = FileInputStream(File(path))
-        val workbook = HSSFWorkbook(file)
-        val dataFormatter = DataFormatter()
+        fun parseXls(path: String): String {
+            val file = FileInputStream(File(path))
+            val workbook = HSSFWorkbook(file)
+            val dataFormatter = DataFormatter()
 
-        var xlsText = ""
+            var xlsText = ""
 
-        for(sheet in workbook){
+            for (sheet in workbook) {
 
-            val sheetName = sheet.sheetName
-            val sheet = workbook.getSheet(sheetName)
+                val sheetName = sheet.sheetName
+                val sheet = workbook.getSheet(sheetName)
 
-            val row = sheet.iterator()
-            while (row.hasNext()) {
-                val currentRow = row.next()
-                val cellsInRow = currentRow.iterator()
-                while (cellsInRow.hasNext()) {
-                    val currentCell = cellsInRow.next()
-                    val cellValue = dataFormatter.formatCellValue(currentCell)
-                    xlsText += cellValue
+                val row = sheet.iterator()
+                while (row.hasNext()) {
+                    val currentRow = row.next()
+                    val cellsInRow = currentRow.iterator()
+                    while (cellsInRow.hasNext()) {
+                        val currentCell = cellsInRow.next()
+                        val cellValue = dataFormatter.formatCellValue(currentCell)
+                        xlsText += cellValue + " "
+                    }
                 }
             }
+            workbook.close()
+            file.close()
+            return xlsText
         }
-        workbook.close()
-        file.close()
-        return xlsText
-    }
 
-    @Throws(Exception::class)
-    fun retryConnection(factory: ConnectionFactory): Connection? {
-        val connectionTrials = 5
-        val waitTime = 5000
-        for (retry in 0..connectionTrials) {
-            try {
-                Thread.sleep((retry * waitTime).toLong())
-                return factory.newConnection()
-            } catch (e: IOException) {
-                println("Unable to establish connection to RabbitMQ. Trial: $retry")
+        @Throws(Exception::class)
+        fun retryConnection(factory: ConnectionFactory): Connection? {
+            val connectionTrials = 5
+            val waitTime = 5000
+            for (retry in 0..connectionTrials) {
+                try {
+                    Thread.sleep((retry * waitTime).toLong())
+                    return factory.newConnection()
+                } catch (e: IOException) {
+                    println("Unable to establish connection to RabbitMQ. Trial: $retry")
+                }
             }
+            println("Unable to establish connection")
+            throw IllegalStateException("No RabbitMQConnection provided")
         }
-        println("Unable to establish connection")
-        throw IllegalStateException("No RabbitMQConnection provided")
     }
-}
 }
