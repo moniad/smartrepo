937af4d52f3169455765cf32533ab3d66a79b140
Mateusz Wieczorek <mateuszw555@wp.pl>
2021-05-24 20:55:00 +0200

    Language Detector works properly.

--- utils/lang_detect/src/main/golang/LanguageDetector.go
+++ utils/lang_detect/src/main/golang/LanguageDetector.go
@@ -2,11 +2,34 @@ package main
 
 import (
 	"fmt"
-
+    "log"
+    "net/http"
+    "encoding/json"
 	"github.com/abadojack/whatlanggo"
 )
 
+type LanguageDetectorPack struct {
+    Phrase string
+}
+
+func languageDetectorService(w http.ResponseWriter, r *http.Request) {
+    var pack LanguageDetectorPack
+    err := json.NewDecoder(r.Body).Decode(&pack)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+	langInfo := whatlanggo.Detect(pack.Phrase)
+	w.Header().Set("Content-Type", "application/json")
+    fmt.Fprintf(w, "{\"phrase\": \"%+v\",\"language\": \"%+v\"}", pack.Phrase, langInfo.Lang.String())
+}
+
+func handleRequests() {
+    http.HandleFunc("/langdetect", languageDetectorService)
+    fmt.Println("Language Detector Service started.")
+    log.Fatal(http.ListenAndServe(":10000", nil))
+}
+
 func main() {
-	info := whatlanggo.Detect("Foje funkcias kaj foje ne funkcias")
-	fmt.Println("Language:", info.Lang.String(), " Script:", whatlanggo.Scripts[info.Script], " Confidence: ", info.Confidence)
+    handleRequests()
 }
